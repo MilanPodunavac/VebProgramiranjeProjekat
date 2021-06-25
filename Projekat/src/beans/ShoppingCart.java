487120d7d72f3e01d95cca2f8aaaeff1159ee734
Aleksandar <aleksandar.radisic@uns.ac.rs>
2021-06-25 19:06:54 +0200

    Ispravak greske -- dodati artikli

--- Projekat/src/beans/ShoppingCart.java
+++ Projekat/src/beans/ShoppingCart.java
@@ -4,12 +4,14 @@ import java.util.*;
 
 public class ShoppingCart {
 	private double totalCost;
-	private Customer customer;
-   
-	public ShoppingCart(double totalCost, Customer customer) {
-		super();
-		this.totalCost = totalCost;
-		this.customer = customer;
+   	private Customer customer;
+   	private java.util.List<Article> articles;
+
+	public ShoppingCart(double totalCost, Customer customer, List<Article> articles) {
+	super();
+	this.totalCost = totalCost;
+	this.customer = customer;
+	this.articles = articles;
 	}
 
 	public double getTotalCost() {
@@ -28,6 +30,44 @@ public class ShoppingCart {
 		this.customer = customer;
 	}
    
-   
+	public java.util.List<Article> getArticles() {
+		if (articles == null)
+			articles = new java.util.Vector<Article>();
+		return articles;
+	}
+	   
+	public java.util.Iterator getIteratorArticles() {
+		if (articles == null)
+			articles = new java.util.Vector<Article>();
+		return articles.iterator();
+	}
+	   
+	public void setArticles(java.util.List<Article> newArticles) {
+		removeAllArticles();
+			for (java.util.Iterator iter = newArticles.iterator(); iter.hasNext();)
+		addArticles((Article)iter.next());
+	}
+	   
+	public void addArticles(Article newArticle) {
+		if (newArticle == null)
+			return;
+		if (this.articles == null)
+			this.articles = new java.util.Vector<Article>();
+		if (!this.articles.contains(newArticle))
+			this.articles.add(newArticle);
+	}
+	   
+	public void removeArticles(Article oldArticle) {
+		if (oldArticle == null)
+			return;
+		if (this.articles != null)
+			if (this.articles.contains(oldArticle))
+				this.articles.remove(oldArticle);
+	}
+	   
+	public void removeAllArticles() {
+		if (articles != null)
+			articles.clear();
+	}
 
 }
