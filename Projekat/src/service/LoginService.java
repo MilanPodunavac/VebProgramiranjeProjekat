56c0e4899f7e39d7b0c66e8fec0dd5c66f7e5954
Aleksandar <aleksandar.radisic@uns.ac.rs>
2021-06-30 21:10:10 +0200

    nzm2.

--- Projekat/src/service/LoginService.java
+++ Projekat/src/service/LoginService.java
@@ -5,12 +5,14 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import beans.User;
 import serialize.AdministratorSerializer;
@@ -23,7 +25,7 @@ import serialize.RestaurantSerializer;
 @Path("")
 public class LoginService {
 	@Context
-	private HttpServletRequest request;
+	private ServletContext context;
 	
 	public LoginService() {
 		
@@ -31,31 +33,55 @@ public class LoginService {
 	
 	@PostConstruct
 	public void init() {
-		if(request.getServletContext().getAttribute("customers") == null) {
-			request.getServletContext().setAttribute("customers", new CustomerSerializer().Load());
+		if(context.getAttribute("customers") == null) {
+			context.setAttribute("customers", new CustomerSerializer().Load());
 		}
-		if(request.getServletContext().getAttribute("administrators") == null) {
-			request.getServletContext().setAttribute("administrators", new AdministratorSerializer().Load());
+		if(context.getAttribute("administrators") == null) {
+			context.setAttribute("administrators", new AdministratorSerializer().Load());
 		}
-		if(request.getServletContext().getAttribute("comments") == null) {
-			request.getServletContext().setAttribute("comments", new CommentSerializer().Load());
+		if(context.getAttribute("comments") == null) {
+			context.setAttribute("comments", new CommentSerializer().Load());
 		}
-		if(request.getServletContext().getAttribute("deliverers") == null) {
-			request.getServletContext().setAttribute("deliverers", new DelivererSerializer().Load());
+		if(context.getAttribute("deliverers") == null) {
+			context.setAttribute("deliverers", new DelivererSerializer().Load());
 		}
-		if(request.getServletContext().getAttribute("managers") == null) {
-			request.getServletContext().setAttribute("managers", new ManagerSerializer().Load());
+		if(context.getAttribute("managers") == null) {
+			context.setAttribute("managers", new ManagerSerializer().Load());
 		}
-		if(request.getServletContext().getAttribute("restaurants") == null) {
-			request.getServletContext().setAttribute("restaurants", new RestaurantSerializer().Load());
+		if(context.getAttribute("restaurants") == null) {
+			context.setAttribute("restaurants", new RestaurantSerializer().Load());
 		}
 	}
 	
-	@GET
+	@POST
 	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public User login(@QueryParam("username")String username) {
-		System.out.println(username);
-		return null;
+	public Response login(User user, @Context HttpServletRequest request) {
+/*		UserDAO userDao = (UserDAO) ctx.getAttribute("userDAO");
+		User loggedUser = userDao.find(user.getUsername(), user.getPassword());
+		if (loggedUser != null) {
+			return Response.status(400).entity("Invalid username and/or password").build();
+		}
+		request.getSession().setAttribute("user", loggedUser);
+		return Response.status(200).build();*/
+		return Response.status(200).build();
+	}
+	
+	
+	@POST
+	@Path("/logout")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public void logout(@Context HttpServletRequest request) {
+		request.getSession().invalidate();
+	}
+	
+	@GET
+	@Path("/currentUser")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User login(@Context HttpServletRequest request) {
+		return (User) request.getSession().getAttribute("user");
 	}
 }
