26327ce5d410f475c376001acb30d5f453c30b11
Aleksandar <aleksandar.radisic@uns.ac.rs>
2021-09-11 00:43:16 +0200

    bugfix

--- Projekat/WebContent/js/restaurant_administrator.js
+++ Projekat/WebContent/js/restaurant_administrator.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$("#restaurant_list").on("click", "tr", function(){
 		let restaurant_name = $(this).children("td:first").text();
+		if(restaurant_name === "") return;
 		let restaurant_location = $(this).children("td:nth-child(3)").text();
 	
 		window.location = "restaurant_administrator.html?name=" + restaurant_name + "&location=" + restaurant_location;
